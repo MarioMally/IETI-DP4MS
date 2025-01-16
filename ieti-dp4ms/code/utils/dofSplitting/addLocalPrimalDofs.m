36ab994c39a0077bf02c4e4d88c4591915435af0
Mario Mally <mario.mally@tu-darmstadt.de>
2025-01-16 13:11:13 +0100

    Corrected bug with wrong selection of primal DOFs

--- ieti-dp4ms/code/utils/dofSplitting/addLocalPrimalDofs.m
+++ ieti-dp4ms/code/utils/dofSplitting/addLocalPrimalDofs.m
@@ -2,7 +2,7 @@ function mp_strct = addLocalPrimalDofs(mp_strct)
     
     for i=1:numel(mp_strct.patch_arr)
         % Define primal Dofs as everything on NI,II but not int tree
-        priDofs = mp_strct.patch_arr(i).G.Edges.IDs(mp_strct.patch_arr(i).G.Edges.Weight == 3 | mp_strct.patch_arr(i).G.Edges.Weight == 6);
+        priDofs = mp_strct.patch_arr(i).G.Edges.IDs(mp_strct.patch_arr(i).G.Edges.Weight == 3 | mp_strct.patch_arr(i).G.Edges.Weight == 4);
         % Remove all Dirichlet Dofs (are set to boundary value strongly)
         priDofs = setdiff(priDofs,mp_strct.patch_arr(i).dir_dofs);
         % Remove all Tree Dofs (are set to zero)
