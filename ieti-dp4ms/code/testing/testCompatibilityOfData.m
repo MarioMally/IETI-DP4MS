c30f583c9590463dedaade85893c292c539c9ae5
Mario Mally <mario.mally@tu-darmstadt.de>
2025-01-17 11:30:54 +0100

    Enabled non-numeric tests of strings in table

--- ieti-dp4ms/code/testing/testCompatibilityOfData.m
+++ ieti-dp4ms/code/testing/testCompatibilityOfData.m
@@ -10,11 +10,23 @@ function bool = testCompatibilityOfData(folderPath,fileName,paperName,tol)
     for iVar=tableRef.Properties.VariableNames
         refVals = tableRef(:,iVar).(1);
         testVals = tableTest(:,iVar).(1);
+        
+        if iscell(refVals)
+            if ~all(cellfun(@(x,y) isequal(x,y),refVals,testVals))
+                bool = false;
+                fprintf('There are not negligiable differences in column  %s!\n',iVar{1});
+                return;
+            end
+        else
+            filter = ~isnan(refVals) & ~(refVals > 1e10);
+            refVals = refVals(filter);
+            testVals = testVals(filter);
 
-        if sum(abs(refVals-testVals)./abs(refVals))/numel(refVals)>tol
-            bool = false;
-            fprintf('There are differences in column  %s!\n',iVar{1});
-            return;
+            if sum(abs(refVals-testVals)./abs(refVals))/numel(refVals)>tol
+                bool = false;
+                fprintf('There are not negligiable differences in column  %s!\n',iVar{1});
+                return;
+            end
         end
     end
 end
